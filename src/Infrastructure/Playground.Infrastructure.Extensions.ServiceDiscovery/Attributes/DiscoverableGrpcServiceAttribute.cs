0124540e769448d764beb5888a8de4a16ad4e2cd
Euan-McVie <euan.mcvie@bradyplc.com>
2020-03-20 11:31:12 +0000

    #7 - Add attributes to flag a service as discoverable.

---
+++ src/Infrastructure/Playground.Infrastructure.Extensions.ServiceDiscovery/Attributes/DiscoverableGrpcServiceAttribute.cs
@@ -0,0 +1,18 @@
+﻿namespace Playground.Infrastructure.Services.ServiceDiscovery.Attributes
+{
+    /// <summary>
+    /// This attribute is used to register the decorated gRPC service with the remote service discovery mechanism.
+    /// </summary>
+    public class DiscoverableGrpcServiceAttribute : DiscoverableServiceAttribute
+    {
+        /// <summary>
+        /// Contructor to set the required properties of a discoverable gRPC service.
+        /// </summary>
+        /// <param name="serviceName">The name of the service to register with service discovery.</param>
+        /// <param name="tags">Additional tags used to group the service.</param>
+        public DiscoverableGrpcServiceAttribute(string serviceName, params string[] tags)
+            : base(serviceName, "gRPC", tags)
+        {
+        }
+    }
+}
---
+++ src/Infrastructure/Playground.Infrastructure.Extensions.ServiceDiscovery/Attributes/DiscoverableServiceAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Playground.Infrastructure.Services.ServiceDiscovery.Attributes
+{
+    /// <summary>
+    /// The <see langword="abstract"/> <see cref="DiscoverableServiceAttribute"/> is used as a base for more specific Discoverable Service attributes.
+    /// This attribute is used to register the decorated service with the remote service discovery mechanism.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    public abstract class DiscoverableServiceAttribute : Attribute
+    {
+        /// <summary>
+        /// The name of the service to register with service discovery.
+        /// </summary>
+        public string ServiceName { get; }
+        /// <summary>
+        /// The type of the service to register with service discovery.
+        /// </summary>
+        public string ServiceType { get; }
+        /// <summary>
+        /// Additional tags used to group the service.
+        /// </summary>
+        public IEnumerable<string> Tags { get; }
+
+        /// <summary>
+        /// Contructor to set the required properties of a discoverable service.
+        /// </summary>
+        /// <param name="serviceName">The name of the service to register with service discovery.</param>
+        /// <param name="serviceType">The type of the service to register with service discovery.</param>
+        /// <param name="tags">Additional tags used to group the service.</param>
+        public DiscoverableServiceAttribute(string serviceName, string serviceType, params string[] tags)
+        {
+            ServiceName = serviceName;
+            ServiceType = serviceType;
+            Tags = tags;
+        }
+    }
+}
